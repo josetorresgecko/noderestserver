a6f52e5a5322f280e25dcbc94d5cd3d56aa36400
josetorresgecko <jose.torres@gecko.com.co>
2020-08-25 13:06:16 -0500

    login normal secc 10

--- server/middledwares/autenticacion.js
+++ server/middledwares/autenticacion.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 //==========================
 let verificaToken = (req, res, next) => {
     let token = req.get('token'); //autorization
+    console.log(token);
     jwt.verify(token, process.env.SEED, (err, decoded) => {
         if (err) {
             return res.status(401).json({
